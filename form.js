499d3e10475023bc9bfeeed2bbdfa017654bc22d
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-19 12:42:21 +0600

    last commit

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-document.getElementById("total-amount").addEventListener('click',function () {
+function addSub() {
     const mainIncome = document.getElementById("income");
     const foodBalanceInput = document.getElementById("food-expense");
     const roombalanceInput = document.getElementById("room-expense");
@@ -8,17 +8,50 @@ document.getElementById("total-amount").addEventListener('click',function () {
     const restBalance = parseInt(mainIncome.value) - parseInt(subTotal);
     document.getElementById('sub-total').innerText = subTotal;
 
-    document.getElementById('Total-balance').innerText = restBalance ;
+    document.getElementById('Total-balance').innerText = restBalance ; 
+}
+function subAdd() {
+    const mainIncome = document.getElementById("income");
+    const foodBalanceInput = document.getElementById("food-expense");
+    const roombalanceInput = document.getElementById("room-expense");
+    const travelBalanceInput = document.getElementById("travel-expense");
+    const subTotal = parseInt(foodBalanceInput.value) + parseInt(roombalanceInput.value) + 
+    parseInt(travelBalanceInput.value);
+    const restBalance = parseInt(mainIncome.value) - parseInt(subTotal);
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById("saving-input");
+    const save = mainIncome.value * .2;
+    const remainigBalance = parseInt(restBalance) - parseInt(save)
+    document.getElementById('saving-total').innerText = save;
+    document.getElementById('Remaining-balance').innerText = remainigBalance;
+
+}
+
+
+
+document.getElementById("total-amount").addEventListener('click',function () {
+    // const mainIncome = document.getElementById("income");
+    // const foodBalanceInput = document.getElementById("food-expense");
+    // const roombalanceInput = document.getElementById("room-expense");
+    // const travelBalanceInput = document.getElementById("travel-expense");
+    // const subTotal = parseInt(foodBalanceInput.value) + parseInt(roombalanceInput.value) + 
+    // parseInt(travelBalanceInput.value);
+    // const restBalance = parseInt(mainIncome.value) - parseInt(subTotal);
+    // document.getElementById('sub-total').innerText = subTotal;
+
+    // document.getElementById('Total-balance').innerText = restBalance ;
 
     // for saving Amount
+    addSub()
+
     document.getElementById("save-btn").addEventListener('click',function () {
+        // document.getElementById("saving-input");
+        // const save = mainIncome.value * .2;
+        // const remainigBalance = parseInt(restBalance) - parseInt(save)
+        // document.getElementById('saving-total').innerText = save;
+        // document.getElementById('Remaining-balance').innerText = remainigBalance;
     
-        document.getElementById("saving-input");
-        const save = mainIncome.value * .2;
-        const remainigBalance = parseInt(restBalance) - parseInt(save)
-        document.getElementById('saving-total').innerText = save;
-        document.getElementById('Remaining-balance').innerText = remainigBalance;
-    
+        subAdd()  
     });
     
 });
